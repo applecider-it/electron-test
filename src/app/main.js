925aaa87fa97bdca910da80d7236c679e417b5b9
owner <owner@localhost>
2026-06-13 13:24:31 +0900

    ディレクトリ整備

--- src/app/main.js
+++ src/app/main.js
@@ -1,17 +1,19 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
+console.log(__dirname);
+
 // ホットリロードの設定（自分自身がある場所 __dirname を監視）
 if (process.env.NODE_ENV !== 'production') {
   require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    electron: path.join(__dirname, '..', 'node_modules', '.bin', 'electron'),
     hardResetMethod: 'exit' // ← ウインドウが残るのを防ぐ設定
   });
 }
 
 // メインプロセス側で実行したい関数を定義
 function myBackendFunction() {
-  console.log('🚨 メインプロセス側の関数が実行されました！(Node.js環境)');
+  console.log('メインプロセス側の関数が実行されました！(Node.js環境)');
 }
 
 // 画面からの合図を待ち受ける
@@ -31,7 +33,7 @@ function createWindow() {
   });
 
   // 画面に表示するHTMLファイルを読み込む
-  win.loadFile('index.html');
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
 
 // Electronの準備ができたらウインドウを開く
