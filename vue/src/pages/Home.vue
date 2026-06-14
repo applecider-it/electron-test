d0b00407a34c506e753b3d87f2108ca9d9d97fca
owner <owner@localhost>
2026-06-14 09:06:19 +0900

    vue-router追加

---
+++ vue/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+const result = ref("")
+
+const testFunc = async () => {
+  result.value = await window.ipcRenderer.invoke('get-data', 'Val1');
+}
+</script>
+
+<template>
+  <h1>Home</h1>
+
+  <div class="card">
+    <button type="button" @click="testFunc">Test</button>
+    <p>
+      result: {{ result }}
+    </p>
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
---
+++ vue/src/pages/Settings.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>設定</h1>
+  </div>
+</template>
