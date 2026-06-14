010e9fa9760b64bda28aaf9f25ef34d4236cdbe7
owner <owner@localhost>
2026-06-14 16:09:06 +0900

    画像動作確認

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import vueSvg from './assets/vue.svg'
+</script>
 
 <template>
   <div class="p-5">
@@ -6,6 +8,7 @@
       <a href="https://electron-vite.github.io" target="_blank">
         <img src="/electron-vite.svg" class="inline-block w-10" />
       </a>
+      <img :src="vueSvg" class="inline-block w-10" />
     </div>
     <div class="space-x-2">
       <router-link to="/" class="app-link-normal">ホーム</router-link>
